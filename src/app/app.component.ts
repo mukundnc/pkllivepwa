7249a8a02d843a9db77f4c8413608420c9df5a65
mukundnc <mukund.nc@gmail.com>
2017-08-07 11:54:51 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { Http } from '@angular/http';
+import { SwipeCardsModule } from 'ng2-swipe-cards';
+// import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  myData: Array<any>;
+  card: any = {};
+  orientation: string = "x";
+  overlay: any = {
+      like: {
+          backgroundColor: '#28e93b'
+      },
+      dislike: {
+          backgroundColor: '#e92828'
+      }
+  };
+
+  constructor(private http:Http) {
+    
+    this.http.get('https://jsonplaceholder.typicode.com/photos')
+      // .map(response => response.json())
+      .subscribe(res => {
+        this.myData = res.json();
+        console.log(this.myData)
+        this.card = {
+          likeEvent: new EventEmitter(),
+          destroyEvent: new EventEmitter(),
+          url: this.myData[1].url
+        }
+      });
+
+  }
+
+  like(like) {
+        console.log(1);
+    }
+
+    onCardLike(event) {
+        console.log(1)
+    }
+
+    onRelease(event) {
+        console.log(1)
+        this.card.url = this.myData[2].url;
+    }
+
+    onAbort(event) {
+        console.log(1)
+    }
+
+    onSwipe(event) {
+        console.log(1)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 
+import { SwipeCardsModule } from 'ng2-swipe-cards';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule,
+    SwipeCardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
